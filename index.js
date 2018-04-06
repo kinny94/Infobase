f32c8fa994946e9796fa52fc4b380468ef965365
kinny94 <ad.sam007@gmail.com>
2018-04-06 17:22:18 -0400

    added server files

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const express = require( 'express' );
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.listen( PORT );
+
+app.listen( PORT, () => {
+    console.log( "Server is running on port 5000 " );
+});

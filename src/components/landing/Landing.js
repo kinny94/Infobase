780f8f0a62f58fd3986eee8b899251e155f09ae3
kinny94 <ad.sam007@gmail.com>
2018-04-05 23:14:26 -0400

    Set up the heirarchy

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends Component{
+    render(){
+        return(
+            <div>
+                <h2>This will be the landing component</h2>
+            </div>
+        )
+    }
+}
+
+export default Landing;

5fc0672ddbe3401df3cbabdbff78e5b2f44bc3cf
kinny94 <ad.sam007@gmail.com>
2018-04-06 23:14:47 -0400

    Added basic component

---
+++ client/src/components/main/github/Github.js
@@ -0,0 +1,15 @@
+import React, { Compponent } from 'react';
+
+class Github extends Component{
+
+    render(){
+        return(
+            <div class="container">
+                <h2>Place github stuff here!!</h2>
+            </div>
+        )
+    }
+
+}
+
+export default Github;
---
+++ client/src/components/main/steam/Steam.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Steam extends Component{
+
+    render(){
+        return(
+            <div>
+                <h2>Place Steam Stuff here!!</h2>
+            </div>
+        )
+    }
+}
+
+export default Steam;
